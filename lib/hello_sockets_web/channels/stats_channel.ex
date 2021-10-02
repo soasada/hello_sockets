f5ceb5d4cd43afca2a29f797327ab864cf581e28
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-10-02 11:40:45 +0200

    Add count to join channel to stats channel

--- lib/hello_sockets_web/channels/stats_channel.ex
+++ lib/hello_sockets_web/channels/stats_channel.ex
@@ -1,18 +1,20 @@
 defmodule HelloSocketsWeb.StatsChannel do
   use Phoenix.Channel
   require Logger
+  alias HelloSockets.Statix
 
   # here we implement authorization
-  def join("user:" <> req_user_id, _payload, socket) do
-    if req_user_id == to_string(socket.assigns.user_id) do
-      {:ok, socket}
-    else
-      Logger.error("#{__MODULE__} failed #{req_user_id} != #{socket.assigns.user_id}")
-      {:error, %{reason: "unauthorized"}}
-    end
+  def join("valid", _payload, socket) do
+    channel_join_increment("success")
+    {:ok, socket}
   end
 
-  def handle_in("ping", _payload, socket) do
-    {:reply, {:ok, %{ping: "pong"}}, socket}
+  def join("invalid", _payload, _socket) do
+    channel_join_increment("fail")
+    {:error, %{reason: "always fail"}}
+  end
+
+  defp channel_join_increment(status) do
+    Statix.increment("channel_join", 1, tags: ["status:#{status}", "channel:StatsChannel"])
   end
 end
